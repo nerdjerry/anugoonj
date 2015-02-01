cb79b93487c6e9ce03d71716583d22ea2b11d651
nerdjerry <psinghal4210@gmail.com>
2015-02-01 22:33:31 +0530

    Adjust footer for all other pages for mobile

--- js/snowmain3d.js
+++ js/snowmain3d.js
@@ -55,14 +55,12 @@
 			}
 			function onDocumentTouchStart( event ) {
 				if ( event.touches.length == 1 ) {
-					event.preventDefault();
 					mouseX = event.touches[ 0 ].pageX - windowHalfX;
 					mouseY = event.touches[ 0 ].pageY - windowHalfY;
 				}
 			}
 			function onDocumentTouchMove( event ) {
 				if ( event.touches.length == 1 ) {
-					event.preventDefault();
 					mouseX = event.touches[ 0 ].pageX - windowHalfX;
 					mouseY = event.touches[ 0 ].pageY - windowHalfY;
 				}
