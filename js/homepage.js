45e612da64fb00f23f2d8226f3f3eef23ff538c1
nerdjerry <psinghal4210@gmail.com>
2015-02-17 17:00:13 +0530

    Add star events css,js,html's responsiveness

--- js/homepage.js
+++ js/homepage.js
@@ -10,6 +10,20 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
 });
     /*Star event image hover*/
+    var mql = window.matchMedia("screen and (max-width: 800px)")
+    if (mql.matches){ // if media query matches
+     $('.starevent').contenthover({
+    overlay_width:240,
+    overlay_height:180,
+    effect:'slide',
+    slide_direction:'left',
+    overlay_x_position:'left',
+    overlay_y_position:'center',
+    overlay_background:'#000',
+    overlay_opacity:0.8
+});
+    }
+    else{
     $('.starevent').contenthover({
     overlay_width:400,
     overlay_height:180,
@@ -20,4 +34,5 @@ $(document).ready(function(){
     overlay_background:'#000',
     overlay_opacity:0.8
 });
+    }
 });
