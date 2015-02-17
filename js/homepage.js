d22c189be4e0429f2cfb4c2069ff8cd694365000
nerdjerry <psinghal4210@gmail.com>
2015-02-17 14:55:19 +0530

    Added dummy star events

--- js/homepage.js
+++ js/homepage.js
@@ -9,4 +9,15 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
 });
+    /*Star event image hover*/
+    $('.starevent').contenthover({
+    overlay_width:400,
+    overlay_height:180,
+    effect:'slide',
+    slide_direction:'left',
+    overlay_x_position:'left',
+    overlay_y_position:'center',
+    overlay_background:'#000',
+    overlay_opacity:0.8
+});
 });
